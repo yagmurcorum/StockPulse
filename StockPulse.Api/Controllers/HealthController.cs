d4d55ec9d465a7663c103b87ed5347c9673c7943
Yağmur Çorum <corumyagmur@hotmail.com>
2026-04-28 14:45:02 +0300

    Add health endpoint for API status checks

---
+++ StockPulse.Api/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockPulse.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            application = "StockPulse.Api",
+            timestampUtc = DateTime.UtcNow
+        });
+    }
+}
